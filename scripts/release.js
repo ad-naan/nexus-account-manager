867066e92b79d853ee1c0d2d3a362b63cd71763b
fzk <458813868@qq.com>
2026-02-27 15:44:34 +0800

    release: v1.0.3

--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const { execSync } = require("child_process");
+import fs from "fs";
+import { execSync } from "child_process";
 
 function updateJSONVersion(file, version) {
   const json = JSON.parse(fs.readFileSync(file, "utf8"));
